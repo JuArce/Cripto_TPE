12c404ecbcc7a2fd8d44e5494183fc3bc1c6fd0f
Julian Arce <juarce@itba.edu.ar>
2022-06-09 00:50:47 -0300

    fix: comment description

--- src/files/file.c
+++ src/files/file.c
@@ -30,7 +30,7 @@ file read_file(FILE * fp, char * filename) {
 		//TODO handle error
 	}
 
-	set_extension(f, strchr(filename, '.')); //The order of this operations is important
+	set_extension(f, strchr(filename, '.')); //The order of these operations is important
 	set_filename(f, strtok(filename, ".")); //Strtok sets '.' to '\0' 
 
 	read_data(fp, f);
